f96a43283c93ad005b929c29752a6fc32055ace1
KeithAquino <aquinokeithvargas@gmail.com>
2023-12-03 02:20:06 +0800

    added new js folders

---
+++ js/ACT29/calculator.js
@@ -0,0 +1,10 @@
+function runActivity() {
+  let first_number;
+  let second_number;
+
+  first_number = Number(prompt("Give me a number."));
+  second_number = Number(prompt("Give me another number."));
+  alert(first_number + second_number);
+
+  console.log(first_number + second_number);
+}
---
+++ js/hello.js
@@ -0,0 +1,7 @@
+function runActivity() {
+  //  alert("Hello, Keith!");
+  let age;
+  age = prompt("How old are you?");
+  console.log("Ah, you're " + age + " years old!");
+  // console.error("ERROR: Incorrect value!");
+}
